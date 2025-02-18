f3a4c76585b77e1ad34a011b692c993607e00874
VADUMr <115543904+VADUMr@users.noreply.github.com>
2025-02-18 22:05:10 +0200

    new projects

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#ffcccb'; // змінюємо колір фону при натисканні на кнопку
+});
